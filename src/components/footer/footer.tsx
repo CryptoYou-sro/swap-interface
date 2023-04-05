cd0b3e092e1c45c3f3030d562ba5ea2951fd4fc9
aliburzhi <aliburzhi033@gmail.com>
2023-04-05 13:02:30 +0300

    refactor: added logic to hide kycLegalModal if user changed account  | ab

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,12 +1,9 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { spacing } from '../../styles';
 import { KycL2LegalModal } from '../../components/modal/kycL2LegalModal';
 import { Button } from '../../components';
-import {
-	KycL2BusinessStatusEnum,
-    useStore
-} from '../../helpers';
+import { KycL2BusinessStatusEnum, useStore } from '../../helpers';
 
 const KYCL2Wrapper = styled.div`
 	margin-top: ${spacing[26]};
@@ -16,7 +13,7 @@ const KYCL2Wrapper = styled.div`
 `;
 
 export const Footer = () => {
-    const {
+	const {
 		state: {
 			isUserVerified,
 			account,
@@ -24,16 +21,20 @@ export const Footer = () => {
 		}
 	} = useStore();
 
-    const [ showKycL2, setShowKycL2 ] = useState(false);
+	const [ showKycL2, setShowKycL2 ] = useState(false);
 
-    return (
-        <KYCL2Wrapper>
-            {isUserVerified && account && ( kycL2Business === KycL2BusinessStatusEnum.INITIAL || kycL2Business === KycL2BusinessStatusEnum.BASIC ) ? (
-                <Button variant="pure" onClick={() => setShowKycL2(true)} color="default">
-                    Verify as a business
-                </Button>
-            ) : null}
-            <KycL2LegalModal showKycL2={showKycL2} updateShowKycL2={setShowKycL2}/>
-        </KYCL2Wrapper>
-    );
+	useEffect(() => {
+		setShowKycL2(false);
+	}, [ account ]);
+
+	return (
+		<KYCL2Wrapper>
+			{isUserVerified && account && ( kycL2Business === KycL2BusinessStatusEnum.INITIAL || kycL2Business === KycL2BusinessStatusEnum.BASIC ) ? (
+				<Button variant="pure" onClick={() => setShowKycL2(true)} color="default">
+					Verify as a business
+				</Button>
+			) : null}
+			<KycL2LegalModal showKycL2={showKycL2} updateShowKycL2={setShowKycL2}/>
+		</KYCL2Wrapper>
+	);
 };
